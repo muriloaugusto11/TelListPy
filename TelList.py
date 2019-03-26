fb07168e3c4fadf84f218aaf8d2c87fb22ad1543
Murilo Augusto <38955899+muriloaugusto11@users.noreply.github.com>
2019-03-26 18:58:24 -0300

    Update TelList.py

--- TelList.py
+++ TelList.py
@@ -3,6 +3,10 @@ class TelList():
     def __init__(self):
         self.contact_list = [] #constructor
 
+    def imp(self):
+      name = input("\n NAME TO REMOVE: ").upper()
+      num = input("\n NUMBER TO REMOVE: ")
+
     def menu(self):
         return int(input('\n TYPE:' +
                            '\n [1]- TO ADD: ' +
@@ -11,6 +15,7 @@ class TelList():
                            '\n [4]- TO EXIT: \n :'))
 
     def show(self):
+        print("\n--FONE-LIST--\n")
         for i in self.contact_list:
             print("NAME: ", i.name)
             print("NUMBER: ", i.num)
@@ -27,25 +32,23 @@ class TelList():
                 num = input('\n TYPE A NUMBER TO ADD: ')
                 contato = Contact(name, num)
                 self.contact_list.append(contato)
-                print("NAME", name, "NUMBER", num, "ADD WITH SUCCESS")
-
+                print("NAME", "(",name,")", "NUMBER", "(",num,")", "ADD WITH SUCCESS")
 
             elif option == 2:
-                print("--FONE-LIST--")
                 self.show()
 
             elif option == 3:
                 self.show()
-                name1 = input("\n NAME TO REMOVE: ").upper()
-                num1 = input("\n NUMBER TO REMOVE: ")
-                contato = Contact(name, num)
+                self.imp()
+                #contato = Contact(name, num)
                 for x in self.contact_list:
-                    if name1 == x.name and num1 == x.num:
+                    if name == x.name and num == x.num:
                         self.contact_list.remove(x)
-                        print("NAME","(",name1,")","AND NUMBER","(",num1,")","REMOVED WITH SUCCESS!")
+                        print("NAME","(",name,")","AND NUMBER","(",num,")","REMOVED WITH SUCCESS!")
+                        self.show()
 
-            if option > 4 or option < 1:
-                print("\n ERROR! SELECT A VALID NUMBER!")
+            elif option > 4 or option < 1:
+              print("\n ERROR! SELECT A VALID NUMBER!")
 
             option = self.menu()
 
